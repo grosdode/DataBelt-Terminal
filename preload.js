50fe9f31483fe5cde6bd6a7a7966fa25d023b069
grosdode <dominik_grosskurth@web.de>
2022-05-06 14:52:38 +0200

    create bridges for file creation

--- preload.js
+++ preload.js
@@ -1,14 +1,25 @@
-// preload.js
+const { contextBridge, ipcRenderer } = require("electron");
 
-// All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
-window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
-
-  for (const dependency of ['chrome', 'node', 'electron']) {
-    replaceText(`${dependency}-version`, process.versions[dependency])
-  }
-})
+// Expose protected methods that allow the renderer process to use
+// the ipcRenderer without exposing the entire object
+contextBridge.exposeInMainWorld("api", {
+  send: (channel, data) => {
+    // whitelist channels
+    let validChannels = [
+      "toMain",
+      "createSensorDocuments",
+      "writeToAccDocument",
+      "writeToTempDocument",
+    ];
+    if (validChannels.includes(channel)) {
+      ipcRenderer.send(channel, data);
+    }
+  },
+  receive: (channel, func) => {
+    let validChannels = ["fromMain", "documentCreated"];
+    if (validChannels.includes(channel)) {
+      // Deliberately strip event as it includes `sender`
+      ipcRenderer.on(channel, (event, ...args) => func(...args));
+    }
+  },
+});
